7f9b953f44768cb763976341cd93390e0d3c6e38
Victor Mours <victor.mours@tigerlilyapps.com>
2015-02-20 14:55:50 +0100

    Working with all, aliased and unaliased

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ aliasdoctor helps you diagnose your alias usage, and answer the following questi
 - What alias should I add to my dotfiles?
 - What alias should I shorten?
 
+
+Recommended Install
+---
+```
+  gem install aliasmanager
+```
+
+For ruby to interact properly with your shell environment, it's easier install aliasmanager through aliases
+
+```
+  alias am="alias -L | aliasmanager"
+```
+
+Usage
+---
+
+`am` For the list of most used commands
+`am unaliased` For the list of most used commands that don't have an alias
+`am aliased` For the list of most used aliases
+
--- lib/aliasmanager.rb
+++ lib/aliasmanager.rb
@@ -1,41 +1,80 @@
-require "open3"
-
 class AliasManager
 
-  def initialize(history_filename)
-    @history_filename = history_filename
+  # Reads the output of `alias -L` in stdin
+  def self.most_used_commands_without_aliases
+    new.most_used_commands_without_aliases
   end
 
-  def most_used_unaliased_commands
+  def most_used_commands_without_aliases
+    put_top_list(commands_without_aliases)
+  end
 
+  def self.most_used_commands
+    new.most_used_commands
+  end
+
+  def most_used_commands
+    put_top_list(full_commands)
+  end
+
+  def self.most_used_aliases
+    new.most_used_aliases
+  end
+
+  def most_used_aliases
+    put_top_list(commands_using_aliases)
   end
 
   private
 
-
-  def commands
-    lines.map do |line| Command.from_history_line(line) end
+  def commands_without_aliases
+    @commands_without_aliases ||= full_commands - commands_using_aliases
   end
 
-  def lines
-    @line ||= File.readlines(File.expand_path(@history_filename))
+  def commands_using_aliases
+    @commands_using_aliases ||= full_commands.clone.keep_if do |command|
+      all_aliases.any? do |aliaz|
+        command.start_with?(aliaz)
+      end
+    end
   end
 
-  Command = Struct.new(:timestamp, :command, :options) do
-
-    def self.from_history_line(line)
-      timestamp = line.split(":")[1].strip
-      full_command = line.split(";").last.strip.split(" ")
-      command = full_command.pop
-      options = full_command
-
-      new(timestamp, command, options)
+  def full_commands
+    @full_commands ||= history.select do |command|
+      command.start_with?(':')
+    end.map do |line|
+      line.split(";").last.gsub("\n", "")
     end
+  end
 
-    def aliased?
+  # lines of zsh_history have the format `: 1424438183:0;which zsh`
+  def history
+    @history ||= File.readlines File.expand_path "~/.zsh_history"
+  end
 
+  def all_aliases
+    @all_aliases ||= all_aliases_commands.map do |alias_command|
+      alias_command.split(" ")[1].split("=").first
     end
+  end
 
+  def all_aliases_commands
+    @all_aliases_commands ||= $stdin.readlines
+  end
+
+  def top_list(commands)
+    commands.group_by { |c| c.split(' ').first }
+      .each_with_object({}) do |(command, occurences), command_counts|
+        command_counts[command] = occurences.count
+      end
+      .to_a.sort do |a,b| b[1] <=> a[1] end
+  end
+
+  def put_top_list(commands)
+    top_list(commands).each do |command_count|
+      puts "#{command_count[1]} #{command_count[0]}"
+    end
   end
 
 end
+
